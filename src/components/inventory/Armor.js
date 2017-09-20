1446872fa0bd560801142ec9d2bb89f7e5f52658
matt-winzer <matt.winzer@gmail.com>
2017-09-19 20:59:13 -0600

    converted Armor component to stateful component

--- src/components/inventory/Armor.js
+++ src/components/inventory/Armor.js
@@ -1,33 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Table, Modal, Header } from 'semantic-ui-react'
 
-const Armor = (props) => {
-  let stealth = 'False'
-  let dexBonus = 'False'
-
-  if (props.stealth) {
-    stealth = 'True'
-  }
-  if (props.dexBonus) {
-    dexBonus = 'True'
+class Armor extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      stealth: 'False',
+      dexBonus: 'False'
+    }
   }
 
-  return (
-    <Modal trigger={<Table.Row>
-                      <Table.Cell>{props.name}</Table.Cell>
-                      <Table.Cell>{props.category}</Table.Cell>
-                      <Table.Cell textAlign='center'>{props.acBase}</Table.Cell>
-                      <Table.Cell textAlign='center'>{dexBonus}</Table.Cell>
-                      <Table.Cell textAlign='center'>{stealth}</Table.Cell>
-                      <Table.Cell textAlign='center'>{props.costValue} {props.costUnit}</Table.Cell>
-                      <Table.Cell textAlign='center'>{props.weight}</Table.Cell>
-                    </Table.Row>} basic size='small' closeIcon>
-      <Header as='h1' icon='shield' content={props.name} />
-      <Modal.Content>
-        <h3>{props.description}</h3>
-      </Modal.Content>
-    </Modal>
-  )
+  componentDidMount() {
+    if (this.props.stealth) {
+      this.setState({
+        ...this.state,
+        stealth: 'True'
+      })
+    }
+    if (this.props.dexBonus) {
+      this.setState({
+        ...this.state,
+        dexBonus: 'True'
+      })
+    }
+  }
+
+  render() {
+    return (
+      <Modal trigger={<Table.Row>
+                        <Table.Cell>{this.props.name}</Table.Cell>
+                        <Table.Cell>{this.props.category}</Table.Cell>
+                        <Table.Cell textAlign='center'>{this.props.acBase}</Table.Cell>
+                        <Table.Cell textAlign='center'>{this.state.dexBonus}</Table.Cell>
+                        <Table.Cell textAlign='center'>{this.state.stealth}</Table.Cell>
+                        <Table.Cell textAlign='center'>{this.props.costValue} {this.props.costUnit}</Table.Cell>
+                        <Table.Cell textAlign='center'>{this.props.weight}</Table.Cell>
+                      </Table.Row>} basic size='small' closeIcon>
+        <Header as='h1' icon='shield' content={this.props.name} />
+        <Modal.Content>
+          <h3>{this.props.description}</h3>
+        </Modal.Content>
+      </Modal>
+    )
+  }
 }
 
 export default Armor
